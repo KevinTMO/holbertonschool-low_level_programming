229c71defcda4803bdaa4810aa04ce83a8d0f0ff
KevinTMO <kevin.ramos3@hotmail.com>
2021-02-03 17:57:58 +0000

    My first programm

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 
+/**
+ *main - Entry point
+ *
+ *Return:Always 0 (Sucess)
+ */
+
+
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
