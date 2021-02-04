7970452f8361a384b58d52b447ebde31b36a3f47
KevinTMO <kevin.ramos3@hotmail.com>
2021-02-04 18:15:57 +0000

    AlphaRev

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ *main - entry point
+ *
+ *Return: Always 0 (Sucess)
+ */
+
+int main(void)
+{
+	int alphrev;
+
+	for (alphrev = 'z'; alphrev <= 'a'; alphrev--)
+	{
+		putchar(num);
+	}
+	putchar('\n');
+	return (0);
+}
