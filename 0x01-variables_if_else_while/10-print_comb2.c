316ce82a7f1712f98b45e0e5a20b2d9b7c182958
KevinTMO <kevin.ramos3@hotmail.com>
2021-02-04 21:32:07 +0000

    Pair of numbers

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ *main - entry point
+ *
+ *Return: Always 0 (Sucess)
+ */
+
+int main(void)
+{
+	int num1;
+	int num2;
+
+	for (num1 = '0'; num1 <= '9'; num1++)
+	{
+		for (num2 = '0'; num2 <= '9'; num2++)
+		{
+			putchar(num1);
+			putchar(num2);
+
+			if (num1 != '9' || num2 != '9')
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
