67cde3b65dbfa9a0362f6c53330f74313ab0efab
KevinTMO <kevin.ramos3@hotmail.com>
2021-02-04 19:24:28 +0000

    Numbers, commas and spaces

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ *main - entry point
+ *
+ *Return: Always 0 (Sucess)
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = '0'; num <= '9'; num++)
+	{
+		putchar(num);
+		if (num != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
