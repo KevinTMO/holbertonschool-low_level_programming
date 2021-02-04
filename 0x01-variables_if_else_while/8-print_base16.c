b2c7ceaf1a69ebb4c76234a8e1d479d7e872cf50
KevinTMO <kevin.ramos3@hotmail.com>
2021-02-04 18:39:44 +0000

    NumbersAndLetters

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,10 +9,15 @@
 int main(void)
 {
 	int num;
+	int ch;
 
 	for (num = 48; num <= 57; num++)
 	{
 		putchar(num);
+	}
+	for (ch = 97; ch <= 102; ch++)
+	{
+		putchar(ch);
 	}
 	putchar('\n');
 	return (0);
