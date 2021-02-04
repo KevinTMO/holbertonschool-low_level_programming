d4ac7502819078eb6cce44f39b68bf63a6e824d0
KevinTMO <kevin.ramos3@hotmail.com>
2021-02-04 17:36:10 +0000

    Alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ *main - entry point
+ *
+ *Return: Always 0 (Sucess)
+ */
+
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n')
+
+		return (0);
+}
