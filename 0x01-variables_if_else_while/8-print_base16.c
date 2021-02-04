73236939c0e99f5d53746ad3979364812e41bbe1
KevinTMO <kevin.ramos3@hotmail.com>
2021-02-04 18:25:43 +0000

    NumberBase16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ *main - entry point
+ *
+ *Return: Always 0 (Sucess)
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 48; num <= 57; num++)
+	{
+		putchar(num);
+	}
+	putchar('\n');
+	return (0);
+}
