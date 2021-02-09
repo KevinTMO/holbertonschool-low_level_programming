86068a9034b2e5e8be940fb282cda32e6bbb968e
KevinTMO <kevin.ramos3@hotmail.com>
2021-02-09 00:00:12 +0000

    Prints the sign of a number

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ *print_sign - will print signs, commas and space after checking the numbers
+ *@n: this is the variable to compare the numbers
+ *Return: will return 1 if positive, -1 if negative and 0 if zero
+ */
+
+int print_sign(int n)
+{
+	if (c < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	if (c > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	if (c == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+}
