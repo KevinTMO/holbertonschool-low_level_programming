248c7985dcb3f8c44c162aa4004ec00cf8900ff1
KevinTMO <kevin.ramos3@hotmail.com>
2021-02-09 05:26:36 +0000

    HolbertonHeader

---
+++ 0x02-functions_nested_loops/holberton.h
@@ -0,0 +1,62 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ *int _putchar will print a single character the same as function putchar
+ *@c is the variable to do it
+ */
+
+int _putchar(char c);
+
+/**
+ *void print_alphabet(void) will prints the alphabet using another main
+ */
+
+void print_alphabet(void);
+
+/**
+ *void print_alphabet_x10 will print the alphabet 10 times
+ */
+
+void print_alphabet_x10(void);
+
+/**
+ *int _islower will check if the characters are lowercase
+ */
+
+int _islower(int c);
+
+/**
+ *int _isalpha will check if the character is uppercase
+ */
+
+int _isalpha(int c);
+
+/**
+ *int print_sign will check if the number is greater or less and print a sign before it
+ */
+
+int print_sign(int n);
+
+/**
+ *_abs will get the absolute value of a number
+ */
+
+int _abs(int n);
+
+/**
+ *print_last_digit - will print the last digit of a number
+ */
+
+int print_last_digit(int);
+
+/**
+ *jack_bauer will print the 24 hours
+ */
+
+void jack_bauer(void);
+
+#endif
