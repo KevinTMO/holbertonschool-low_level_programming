1062ac4597be651ede9b5900c6daa86ebe553f0c
KevinTMO <kevin.ramos3@hotmail.com>
2021-02-09 18:14:01 +0000

    Addition of two integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ *add - prototype will add two integers and return the result
+ *@a: will hold an integer
+ *@b: will hold an integer
+ *Return: will return the result of the addition
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
