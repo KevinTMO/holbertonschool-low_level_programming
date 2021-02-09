b3759c5649921ba59cd05ac20012f79301f9be54
KevinTMO <kevin.ramos3@hotmail.com>
2021-02-09 03:57:48 +0000

    24Hours

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ *jack_bauer - will print the 24 hours in long format
+ *
+ * Return: will not return a value (void)
+ */
+
+void jack_bauer(void)
+{
+	int hours;
+	int minutes;
+
+	for (hours = 0; hours < 24; hours++)
+	{
+		for (minutes = 0; minutes <= 59; minutes++)
+		{
+			_putchar(hours / 10 + '0');
+			_putchar(hours % 10 + '0');
+			_putchar(':');
+			_putchar(minutes / 10 + '0');
+			_putchar(minutes % 10 + '0');
+			_putchar('\n');
+		}
+	}
+}
