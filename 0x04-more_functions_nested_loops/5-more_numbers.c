c902ec236e3a9806923094a8557a930e33f3b1b0
KevinTMO <kevin.ramos3@hotmail.com>
2021-02-11 18:58:18 +0000

    More_numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -14,11 +14,11 @@ void more_numbers(void)
 	{
 		for (number = 0; number <= 14; number++)
 		{
-			if (number < 10)
+			if (number <= 9)
 			{
 				_putchar(number + '0');
 			}
-			else if (number > 9)
+			else if (number >= 10)
 			{
 				_putchar((number / 10) + '0');
 				_putchar((number % 10) + '0');
