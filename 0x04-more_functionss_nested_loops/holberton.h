c361a635715ff8c07594e0a0588ab1877e2c4989
KevinTMO <kevin.ramos3@hotmail.com>
2021-02-11 13:45:42 +0000

    Prototypes

---
+++ 0x04-more_functionss_nested_loops/holberton.h
@@ -0,0 +1,17 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+/**
+ *_putchar will print a single character
+ */
+
+int _putchar(char c);
+
+/**
+ *_isupper will check for uppercase characters
+ */
+
+int _isupper(int c);
+
+
+#endif
