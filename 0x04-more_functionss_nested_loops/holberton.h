70b65759ded801a376da2702a72a2f0379a96843
KevinTMO <kevin.ramos3@hotmail.com>
2021-02-11 17:37:18 +0000

    Updated_header

--- 0x04-more_functionss_nested_loops/holberton.h
+++ 0x04-more_functionss_nested_loops/holberton.h
@@ -19,5 +19,34 @@ int _isupper(int c);
 
 int _isgidit(int c);
 
+/**
+ *mul will multiply two variables
+ */
+
+int mul(int a, int b);
+
+/**
+ *print_numbers will print numbers from 0 to 9
+ */
+
+void print_numbers(void);
+
+/**
+ *print_most_numbers will print numbers from 0 to 9 except for 2 and 4
+ */
+
+void print_most_numbers(void);
+
+/**
+ *more_numbers will print numbers from 0 to 14 ten times
+ */
+
+void more_numbers(void);
+
+/**
+ *print_lines will print lines depending on the value of n
+ */
+
+void print_line(int n);
 
 #endif
