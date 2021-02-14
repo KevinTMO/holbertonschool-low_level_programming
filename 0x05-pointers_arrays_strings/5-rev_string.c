655e5be360b4ea9eda725185cf6ee73d2bfa511b
KevinTMO <kevin.ramos3@hotmail.com>
2021-02-14 17:29:44 +0000

    Reverse strings

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,31 @@
+include "holberton.h"
+
+/**
+ *rev_string - will print the strings of the pointer in reverse order
+ *@s: the pointer holding the string
+ *Return: will return nothing (void)
+ */
+
+void rev_string(char *s)
+{
+	int index, sindex;
+	char firstchar, lastchar;
+
+	sindex = 0;
+
+	for (index = 0; s[index] != '\0'; index++)
+	{
+		;
+	}
+
+	index--;
+
+	for (; index >= sindex; index--)
+	{
+		lastchar = s[index];
+		firstchar = s[sindex];
+		s[sindex] = lastchar;
+		s[index] = firstchar;
+		sindex++;
+	}
+}
