4f7d29c18528375778e751553037894ad085963c
KevinTMO <kevin.ramos3@hotmail.com>
2021-02-14 01:28:29 +0000

    Reset to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ *reset_to_98 - will redefine the value of n with a pointer
+ *@n: is the pointer that got the address and value of n
+ *Return: will return nothing (void)
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
