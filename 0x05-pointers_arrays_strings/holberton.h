5f5c7fd04caa34033a962e4d50063a1f0066c736
KevinTMO <kevin.ramos3@hotmail.com>
2021-02-14 01:25:03 +0000

    Prototype Header

---
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -0,0 +1,18 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stdio.h>
+
+/**
+ *_putchar will print a single character
+ */
+
+int _putchar(char c);
+
+/**
+ *reset_to_98 will redefine the value of the variable n with a pointer
+ */
+
+void reset_to_98(int *n);
+
+#endif
