f93961743c54b31bdae829015b411b63c8f2a60a
KevinTMO <kevin.ramos3@hotmail.com>
2021-02-15 06:16:07 +0000

    Puts_half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+
+/**
+ *puts_half - print the other half of a string
+ *@str: the pointer holding the string
+ *Return: will return nothing (void)
+ */
+
+void puts_half(char *str)
+{
+	int index;
+	int sindex;
+
+	for (index = 0; str[index] != '\0'; index++)
+	{
+		;
+	}
+	if (index % 2 == 0)
+	{
+		sindex = (index / 2);
+	}
+	else
+	{
+		sindex = ((index - 1) / 2);
+	}
+	for (; sindex < index; sindex++)
+	{
+		_putchar(str[sindex]);
+	}
+}
