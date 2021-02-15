6ed8412af7230ddb72ea3304ec4fe3d747422ee4
KevinTMO <kevin.ramos3@hotmail.com>
2021-02-15 22:25:46 +0000

    Copy&Paste&Return

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ *_strcpy - will copy the string of a pointer to another pointer
+ *@dest: is the pointer to pasty the copied string
+ *@src: is the pointer that hold the string to get copy
+ *Return: will return dest value
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	dest = src;
+
+	return (dest);
+}
