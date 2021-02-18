cb4aa72e8f2266af90e28e6c9e6b24d3e6251b33
KevinTMO <kevin.ramos3@hotmail.com>
2021-02-18 17:22:13 +0000

    Headerfile

---
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -0,0 +1,16 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+/**
+ *It works the same as putchar from stdio.h
+ */
+
+int _putchar(char c);
+
+/**
+ *_strcat - will concatenate two strings using two pointers
+ */
+
+char *_strcat(char *dest, char *src);
+
+#endif
