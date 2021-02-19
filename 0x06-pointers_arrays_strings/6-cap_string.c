2a4c44a194c12ae0a095b7bb480568c0d379b2f7
KevinTMO <kevin.ramos3@hotmail.com>
2021-02-19 23:40:05 +0000

    Capitalize

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -3,35 +3,35 @@
 /**
  *cap_string - will capitalize all first character of every word
  *@s: is the variable pointing to the array with the strings
- *Return: will return result of the pointer
+ *Return: will return the result of the pointer
  */
 
 char *cap_string(char *s)
 {
 	int index;
+	int sindex;
+	char sp[] = {'\"', ' ', '?', '!', '\n', '\t', '(', ')', '.',
+		     '*', '+', '#', '%'};
 
-	for (index = 0; s[index] != '0'; index++)
+	index = 0;
+
+	if (s[index] >= 'a' && s[index] <= 'z')
 	{
-		if (index == 0)
-		{
-			if (s[index] >= 'a' && s[index] <= 'z')
-				s[index] = s[index] - 32;
-			continue;
-		}
-		if (s[index] == ' ')
-		{
-			index++;
+		s[index] = s[index] - 32;
+		index++;
+	}
 
+	for (; s[index] != '\0'; index++)
+	{
+		for (sindex = 0; sp[sindex] != '\0'; sindex++)
+		{
 			if (s[index] >= 'a' && s[index] <= 'z')
 			{
-				s[index] = s[index] - 32;
-				continue;
+				if (s[index - 1] == sp[sindex])
+				{
+					s[index] = s[index] - 32;
+				}
 			}
-		}
-		else
-		{
-			if (s[index] >= 'A' && s[index] <= 'Z')
-				s[index] = s[index] + 32;
 		}
 	}
 	return (s);
