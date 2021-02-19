058d942a6d30003e1cd9610bc9a724a20de75d48
KevinTMO <kevin.ramos3@hotmail.com>
2021-02-19 00:12:04 +0000

    Updated

--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -19,4 +19,10 @@ char *_strcat(char *dest, char *src);
 
 char *_strncat(char *dest, char *src, int n);
 
+/**
+ *_strncpy - will copy one string to another and append the rest with null byte
+ */
+
+char *_strncpy(char *dest, char *src, int n);
+
 #endif
