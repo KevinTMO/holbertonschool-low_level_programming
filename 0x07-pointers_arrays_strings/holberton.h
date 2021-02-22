e4b9d994f049239b17435b63a9667e7c5d3ebd9a
KevinTMO <kevin.ramos3@hotmail.com>
2021-02-22 22:34:45 +0000

    Uptade

--- 0x07-pointers_arrays_strings/holberton.h
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -19,4 +19,10 @@ char *_memset(char *s, char b, unsigned int n);
 
 char *_memcpy(char *dest, char *src, unsigned int n);
 
+/**
+ *_strchr - will iterate until c value is met
+ */
+
+char *_strchr(char *s, char c);
+
 #endif
