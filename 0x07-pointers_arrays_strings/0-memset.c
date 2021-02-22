0a0b9dbfa44d5c895b5818202e93666592a97227
KevinTMO <kevin.ramos3@hotmail.com>
2021-02-22 18:35:50 +0000

    Memory fill

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ *_memset - will fill the memory block that is pointing
+ *@s: is the first parameter that point to the memory block to fill
+ *@b: is the second parameter to pass the memory to the pointer
+ *@n: is the numbers of the memory that is passing
+ *Return: will return the result of the pointer
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int index;
+
+	if (n > 0)
+	{
+		for (index = 0; index < n; index++)
+		{
+			s[index] = b;
+		}
+	}
+	return (s);
+}
