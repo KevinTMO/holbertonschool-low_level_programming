3926519586a7a4c2cc79dff2ed060b9e7fcf1d8f
KevinTMO <kevin.ramos3@hotmail.com>
2021-02-22 18:32:09 +0000

    Header

---
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -0,0 +1,18 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stdio.h>
+
+/**
+ *same as putchar function
+ */
+
+int _putchar(char c);
+
+/**
+ *_memset - will fill a memory block
+ */
+
+char *_memset(char *s, char b, unsigned int n);
+
+#endif 
