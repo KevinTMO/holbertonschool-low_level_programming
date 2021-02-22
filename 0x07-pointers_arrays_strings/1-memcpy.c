781d68938767739875ba204d622f0601adf20e57
KevinTMO <kevin.ramos3@hotmail.com>
2021-02-22 19:52:10 +0000

    Hot fix

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -12,7 +12,7 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int index;
 
-	for (index = 0; src[index] <= n; index++)
+	for (index = 0; index <= n; index++)
 	{
 		dest[index] = src[index];
 	}
