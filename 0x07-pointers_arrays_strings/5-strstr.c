ffbd5efdaf135f74f53abb1949f6acbbea186272
KevinTMO <kevin.ramos3@hotmail.com>
2021-02-23 03:27:20 +0000

    Locate the substring of a longer string

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ *_strstr- will locate the substring of a longer string
+ *@haystack: is the string to be scanned
+ *@needle: is the string containing the characters to match
+ *Return: will return the result of the pointer or NULL
+ */
+
+
+char *_strstr(char *haystack, char *needle)
+{
+	int index;
+	int sindex;
+
+	for (index = 0; haystack[index] != '\0'; index++)
+	{
+		for (sindex = 0; needle[sindex] != '\0'; sindex++)
+		{
+			if (needle[sindex] != haystack[index])
+			{
+				return (&needle[sindex]);
+			}
+		}
+	}
+	return (0);
+}
