1696dbe6243268aad5281e6b1aaa0a0262c85b3a
KevinTMO <kevin.ramos3@hotmail.com>
2021-02-24 19:10:06 +0000

    Header

---
+++ 0x08-recursion/holberton.h
@@ -0,0 +1,16 @@
+#indef HOLBERTON_H
+#define HOLBERTON_H
+
+/**
+ *_putchar - it works as the original putchar
+ */
+
+int _putchar(char c);
+
+/**
+ *puts_recursion will iterate calling the same function until the if comes true
+ */
+
+void _puts_recursion(char *s);
+
+#endif
