99163de5d00a83e61544685ccadbcf80e76dfba1
KevinTMO <kevin.ramos3@hotmail.com>
2021-02-27 22:27:24 +0000

    Adding return to the first statement

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -12,16 +12,17 @@
 
 int main(int argc, char *argv[])
 {
-	int index = 1;
+	int index;
 	int sum = 0;
 
 	if (argc < 2)
 	{
 		printf("0\n");
+		return (0);
 	}
 	else
 	{
-		for (; argv[index];)
+		for (index = 1; argv[index]; index++)
 		{
 			if (isdigit(*argv[index]) == 0)
 			{
@@ -29,7 +30,6 @@ int main(int argc, char *argv[])
 				return (1);
 			}
 			sum += atoi(argv[index]);
-			index++;
 		}
 		printf("%d\n", sum);
 	}
