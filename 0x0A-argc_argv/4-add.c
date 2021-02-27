1111863d80d88612aa663ddd3cd6fb93295ea51c
KevinTMO <kevin.ramos3@hotmail.com>
2021-02-27 02:00:14 +0000

    Will summ arguments if is digit

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ *main - argc (argument count) && argv (argument vector)
+ *@argc: argument count
+ *@argv: argument vector
+ *Return: 0 always (sucess)
+ */
+
+int main(int argc, char *argv[])
+{
+	int index;
+	int sum = 0;
+
+	if (argc < 1)
+	{
+		printf("0\n");
+	}
+	for (index = 1; argv[index]; index++)
+	{
+		if (isdigit(*argv[index]) == 0)
+		{
+			printf("Error\n");
+			return (1);
+		}
+		sum += atoi(argv[index]);
+	}
+	printf("%d\n", sum);
+
+	return (0);
+}
