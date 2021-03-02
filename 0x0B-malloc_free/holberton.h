43040e5136992718e7d827b81f6ffa1533547f2b
KevinTMO <kevin.ramos3@hotmail.com>
2021-03-02 20:46:20 +0000

    Header

---
+++ 0x0B-malloc_free/holberton.h
@@ -0,0 +1,18 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+/**
+ *_PUTCHAR will do as the regular putchar do
+ */
+
+int _putchar(char c);
+
+/**
+ *create_array - will create an array of chars with a specific char
+ */
+
+char *create_array(unsigned int size, char c);
+
+
+
+#endif
