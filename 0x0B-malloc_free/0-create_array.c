f31d17eb8d3989fc2574a583dea941d3949e500e
KevinTMO <kevin.ramos3@hotmail.com>
2021-03-02 20:48:25 +0000

    Add a specific character to an array using malloc

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+#include "stdlib.h"
+
+/**
+ *create_array - will create an array of chars with a specific char
+ *@size: is the size of the array
+ *@c: is the specific character
+ *Return: will return the pointer
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *ch;
+	unsigned int index;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+
+	ch = malloc(sizeof(*ch) * size);
+
+	for (index = 0; index < size; index++)
+	{
+		ch[index] = c;
+	}
+	ch[index] = '\0';
+
+	return (ch);
+	free(ch);
+}
