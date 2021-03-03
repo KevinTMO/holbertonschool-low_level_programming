2a5b0be60838c5a35c7db6874bf1cf0525e6b8d2
KevinTMO <kevin.ramos3@hotmail.com>
2021-03-03 00:01:36 +0000

    Copy one string to another array

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,45 @@
+include "holberton.h"
+#include "stdlib.h"
+
+/**
+ *_strdup - will copy the string of on array to another using mallocs
+ *to get the size first
+ *@str: str is the pointer pointing to the string to copy
+ *Return: will return the pointer
+ */
+
+char *_strdup(char *str)
+{
+	char *ch;
+	int index;
+	int sindex;
+
+	if (str == 0)
+	{
+		return (NULL);
+	}
+
+	for (index = 0; str[index] != '\0'; index++)
+	{
+		;
+	}
+
+	ch = malloc(sizeof(char) * index);
+
+	if (ch == 0)
+	{
+		return (NULL);
+	}
+	else
+	{
+		for (sindex = 0; sindex < index; sindex++)
+		{
+			ch[sindex] = str[sindex];
+		}
+	}
+
+	ch[sindex] = '\0';
+
+	return (ch);
+	free(ch);
+}
