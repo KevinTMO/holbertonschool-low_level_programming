4bb1330f69bb624e1dbaf2871f2b652ee70de5b8
KevinTMO <kevin.ramos3@hotmail.com>
2021-03-04 21:13:12 +0000

    Updated

--- 0x0C-more_malloc_free/holberton.h
+++ 0x0C-more_malloc_free/holberton.h
@@ -2,10 +2,21 @@
 #define HOLBERTON_H
 
 /**
+ *_strlen - will get the lenght of a string
+ */
+
+int _strlen(char *s);
+
+/**
  *malloc_checked - will allocated memory using malloc
  */
 
 void *malloc_checked(unsigned int b);
 
+/**
+ *string_nconcat - will concatenate two strings in one new string
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n);
 
 #endif
