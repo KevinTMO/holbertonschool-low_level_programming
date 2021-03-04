04bf5d5624b00915bfc459d5c156a0c1e3674bd8
KevinTMO <kevin.ramos3@hotmail.com>
2021-03-04 19:48:23 +0000

    Task #0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+#include "stdlib.h"
+
+/**
+ *malloc_checked - will allocated memory using malloc
+ *@b: is the variable holding the numbers
+ *@mem: is the pointer getting the malloc function
+ *Return: will return the pointer or exit if null with 98
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *mem;
+
+	mem = malloc(b);
+
+	if (mem == NULL)
+	{
+		exit(98);
+	}
+	return (mem);
+}
