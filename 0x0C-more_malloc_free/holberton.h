69794ef6ececc1f70aaff134877c7f907d982287
KevinTMO <kevin.ramos3@hotmail.com>
2021-03-04 23:07:07 +0000

    Updated

--- 0x0C-more_malloc_free/holberton.h
+++ 0x0C-more_malloc_free/holberton.h
@@ -19,4 +19,10 @@ void *malloc_checked(unsigned int b);
 
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 
+/**
+ *_calloc - will set the memory to zero
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size);
+
 #endif
