25cc9cee8de73e5140073caec4a6363217bf5baa
KevinTMO <kevin.ramos3@hotmail.com>
2021-03-04 03:27:39 +0000

    Will concatenate arguments

---
+++ 0x0B-malloc_free/5-argstostr.c
@@ -0,0 +1,51 @@
+#include "holberton.h"
+#include "stdlib.h"
+
+/**
+ *argstostr - this will concatenate arguments strings
+ *@ac: is the argument counts
+ *@av: is argument vectors
+ *Return: will return the pointer
+ */
+
+char *argstostr(int ac, char **av)
+{
+	char *ch;
+	int index, sindex, result;
+
+	if (ac == 0 || av == NULL)
+	{
+		return (0);
+	}
+
+	for (index = 0; index < ac; index++)
+	{
+		for (sindex = 0; av[index][sindex]; sindex++)
+		{
+			;
+		}
+		result = 0;
+		result += sindex + 1;
+	}
+
+	ch = malloc(sizeof(char) * result + 1);
+
+	if (!ch)
+	{
+		return (0);
+	}
+
+	result = 0;
+
+	for (index = 0; index < ac; index++)
+	{
+		for (sindex = 0; av[index][sindex]; sindex++, result++)
+		{
+			ch[result] = av[index][sindex];
+		}
+		ch[result] = '\n';
+		result++;
+	}
+
+	return (ch);
+}
