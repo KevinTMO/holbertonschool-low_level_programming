46983de4064671b8cfd1587ce1f6bf728610cecb
KevinTMO <kevin.ramos3@hotmail.com>
2021-03-05 00:09:22 +0000

    Updated

--- 0x0C-more_malloc_free/holberton.h
+++ 0x0C-more_malloc_free/holberton.h
@@ -25,4 +25,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n);
 
 void *_calloc(unsigned int nmemb, unsigned int size);
 
+/**
+ *array_range - will print an array of integers in hex
+ */
+
+int *array_range(int min, int max);
+
 #endif
