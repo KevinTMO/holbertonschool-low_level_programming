95e73c0a0060a5afcdb090f0b6a5d1aef0cd1eed
KevinTMO <kevin.ramos3@hotmail.com>
2021-03-09 03:20:59 +0000

    Fixing free variable of second if statement

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -81,8 +81,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 		cowner = malloc(_strlen(owner) + 1);
 		if (cowner == 0)
 		{
-			free(cowner);
 			free(cname);
+			free(ndog);
 			return (0);
 		}
 		ndog->owner = _strcpy(cowner, owner);
