1e2d0cc0a09b160559377971bc89211d6bfb67c3
KevinTMO <kevin.ramos3@hotmail.com>
2021-03-09 00:15:53 +0000

    Data for your dog using structures

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ *struct dog - will get the data name, age and owner
+ *@name: name of the dog
+ *@age: age of the dog
+ *@owner: owner name of the dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
