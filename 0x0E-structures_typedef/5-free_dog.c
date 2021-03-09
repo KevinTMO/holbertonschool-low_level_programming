63bfaaec635fd7e392a00c6a5a93bb876c203886
KevinTMO <kevin.ramos3@hotmail.com>
2021-03-09 03:51:25 +0000

    Fixing betty

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -2,9 +2,9 @@
 #include "dog.h"
 
 /**
- *
- *
- *
+ *free_dog - will free all the variable that got memory
+ *@d: is the pointer pointing to those variables
+ *Return: will return nothing (void)
  */
 
 void free_dog(dog_t *d)
