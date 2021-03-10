a101f4bdf699252d8220f8e0d11da00ed63b9ee3
KevinTMO <kevin.ramos3@hotmail.com>
2021-03-10 20:04:08 +0000

    Adding comments to each parameter and function

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,10 +1,11 @@
 #include "function_pointers.h"
 
 /**
- *
- *
- *
- *
+ *array_iterator - will iterate inside an array using a pointer func
+ *@array: is the array with the values
+ *@size: is the size using a typedef sizeof
+ *@action: is pointing to the functions to print
+ *Return: will return void (nothing)
  */
 
 void array_iterator(int *array, size_t size, void (*action)(int))
