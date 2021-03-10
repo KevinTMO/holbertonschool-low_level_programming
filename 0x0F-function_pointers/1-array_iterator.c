e94d3c768323d8c5e3e566cd7390a795d306e7e5
KevinTMO <kevin.ramos3@hotmail.com>
2021-03-10 21:01:39 +0000

    Testing

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -12,11 +12,6 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int index;
 
-	if (array == 0 || action == 0)
-	{
-		return;
-	}
-
 	for (index = 0; index < size; index++)
 	{
 		action(array[index]);
