f5cd9762ee38a5e84d4f3ff3a73df01f1a860525
KevinTMO <kevin.ramos3@hotmail.com>
2021-03-11 19:30:18 +0000

    Will print arguments after comma and space

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,33 @@
+#include "variadic_functions.h"
+
+/**
+ *print_numbers - will print all the arguments after n with separator
+ *@separator: is the comma and space after arguments
+ *@n: is the total of arguments
+ *Return: will return nothing (void)
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int index;
+
+	va_list args;
+
+	if (args == 0 || n == 0)
+	{
+		return;
+	}
+
+	va_start(args, n);
+
+	for (index = 0; index < n; index++)
+	{
+		printf("%d", va_arg(args, int));
+
+		if (separator != '\0' && index + 1 != n)
+		{
+			printf("%s", separator);
+		}
+	}
+	printf("\n");
+}
