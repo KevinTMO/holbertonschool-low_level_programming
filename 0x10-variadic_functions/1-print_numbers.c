54642b6dd63a5209f3f4249b698a9e81e5cf1241
KevinTMO <kevin.ramos3@hotmail.com>
2021-03-11 19:32:44 +0000

    Eliminating two statements for testing

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -13,11 +13,6 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	va_list args;
 
-	if (args == 0 || n == 0)
-	{
-		return;
-	}
-
 	va_start(args, n);
 
 	for (index = 0; index < n; index++)
