6400a1b497d5dabb4f65157b7b8748b9636bc157
KevinTMO <kevin.ramos3@hotmail.com>
2021-03-12 03:48:33 +0000

    Updated

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -3,9 +3,17 @@
 
 #include <stdarg.h>
 #include <stdio.h>
+#include <string.h>
+
+typedef struct opfunc
+{
+	char *opsym;
+	void (*f)(va_list);
+}fname_t;
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
 
 #endif /* VAR_H */
