34648235d4fb6357eedf08d477f019e2b763c7c1
KevinTMO <kevin.ramos3@hotmail.com>
2021-03-19 13:52:16 +0000

    Print before main is executed

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ *printbefore - will print before the main is executed
+ *Return: will return nothing (void)
+ */
+
+void printbefore(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
