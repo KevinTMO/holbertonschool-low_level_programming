a6169180432fa96160d653f47b742d0ad6f9cc9a
KevinTMO <kevin.ramos3@hotmail.com>
2021-03-21 19:27:28 +0000

    Fixing statements and while body

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,18 +10,17 @@ size_t print_listint(const listint_t *h)
 {
 	int count = 0;
 
+	if (h == NULL)
+	{
+		return (0);
+	}
 	while (h != NULL)
 	{
-		if (h == NULL)
-		{
-			return (0);
-		}
-		else
-		{
-			printf("%d\n", h->n);
-		}
-		h = h->next;
-		count++;
+
+	printf("%d\n", h->n);
+	h = h->next;
+	count++;
+
 	}
 	return (count);
 }
