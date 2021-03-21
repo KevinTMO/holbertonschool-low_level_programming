3ce813ce5216b8ccf0227c8d534c314564de5e2d
KevinTMO <kevin.ramos3@hotmail.com>
2021-03-21 17:55:55 +0000

    Changing check malloc statement for a diff pos

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -20,12 +20,12 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	new_node = malloc(sizeof(listint_t));
 
-	new_node->n = n;
-
 	if (new_node == NULL)
 	{
-		return (NULL);
+		return (0);
 	}
+
+	new_node->n = n;
 
 	if (idx == 0)
 	{
