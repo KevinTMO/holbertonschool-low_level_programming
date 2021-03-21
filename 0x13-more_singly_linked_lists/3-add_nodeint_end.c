4bf0721629a7f93321774832d2b316efa81e8a62
KevinTMO <kevin.ramos3@hotmail.com>
2021-03-21 17:52:24 +0000

    Adding a check statement after malloc

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -13,6 +13,11 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 
 	new_node = malloc(sizeof(listint_t));
 
+	if (new_node == NULL)
+	{
+		return (0);
+	}
+
 	new_node->n = n;
 	new_node->next = NULL;
 
