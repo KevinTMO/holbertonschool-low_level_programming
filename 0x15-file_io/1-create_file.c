3d8aa322b8253cb22480188e842c7a557589c06f
KevinTMO <kevin.ramos3@hotmail.com>
2021-03-30 22:33:30 +0000

    Samething with another name... cuz Holberton

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -28,7 +28,7 @@ int create_file(const char *filename, char *text_content)
 {
 	int fd, _write;
 
-	if (!(filename) && !(text_content))
+	if (!filename)
 	{
 		return (-1);
 	}
@@ -37,6 +37,12 @@ int create_file(const char *filename, char *text_content)
 
 	if (fd == -1)
 	{
+		return (-1);
+	}
+
+	if (!text_content)
+	{
+		close(fd);
 		return (-1);
 	}
 
