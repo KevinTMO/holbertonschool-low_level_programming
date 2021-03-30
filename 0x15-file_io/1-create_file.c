6cafe77459186681b3a362ed78c8a940f8d7d0b8
KevinTMO <kevin.ramos3@hotmail.com>
2021-03-30 22:15:47 +0000

    Changing pos in fd

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -28,27 +28,21 @@ int create_file(const char *filename, char *text_content)
 {
 	int fd, _write;
 
-	if (filename == NULL)
+	if (filename == NULL && text_content == NULL)
 	{
 		return (-1);
 	}
 
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 
 	if (fd == -1)
 	{
 		return (-1);
 	}
 
-	if (text_content == NULL)
-	{
-		close(fd);
-		return (-1);
-	}
-
 	_write = write(fd, text_content, _strlen(text_content));
 
-	if (_write == -1 || _write != _strlen(text_content))
+	if (_write == -1)
 	{
 		return (-1);
 	}
