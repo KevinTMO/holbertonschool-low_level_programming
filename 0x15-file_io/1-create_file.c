bb88d0bfdc8325e2be4445de1488ab9bf4393f84
KevinTMO <kevin.ramos3@hotmail.com>
2021-03-30 22:17:47 +0000

    Trying more parameters

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -33,7 +33,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	}
 
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 
 	if (fd == -1)
 	{
