a9ec596afeef4dee252bd011062ef648d54cd2b8
KevinTMO <kevin.ramos3@hotmail.com>
2021-03-30 22:39:17 +0000

    Fixing one statement

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -42,8 +42,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (!text_content)
 	{
-		close(fd);
-		return (-1);
+		text_content = "";
 	}
 
 	_write = write(fd, text_content, _strlen(text_content));
