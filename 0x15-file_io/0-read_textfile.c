948d4e31f251220c7459f66b7e6e004402e680c0
KevinTMO <kevin.ramos3@hotmail.com>
2021-03-30 19:33:43 +0000

    Fixing fd statement

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -17,36 +17,34 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	{
 		return (0);
 	}
-
 	fd = open(filename, O_RDONLY);
-
 	if (fd == -1)
 	{
-		return (1);
-	}
-
-	buffer = malloc(sizeof(char) * letters);
-
-	if (buffer == NULL)
-	{
-		close(fd);
 		return (0);
 	}
-
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+	{
+		return (0);
+	}
 	_read = read(fd, buffer, letters);
+	{
 		if (_read == -1)
 		{
 			free(buffer);
 			close(fd);
 			return (0);
 		}
-	_write = write(1, buffer, _read);
+	}
+	_write = write(STDOUT_FILENO, buffer, _read);
+	{
 		if (_write == -1)
 		{
 			free(buffer);
 			close(fd);
 			return (0);
 		}
+	}
 	close(fd);
 	return (_write);
 }
