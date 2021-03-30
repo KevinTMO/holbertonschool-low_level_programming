3bc3ec3b48c6b1a2f5de0c4d53701938399e72f3
KevinTMO <kevin.ramos3@hotmail.com>
2021-03-30 23:28:01 +0000

    Append the data to a file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -40,6 +40,11 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (-1);
 	}
 
+	if (text_contet == NULL)
+	{
+		return (-1);
+	}
+
 	_write = write(fd, text_content, _strlen(text_content));
 
 	if (_write == -1)
