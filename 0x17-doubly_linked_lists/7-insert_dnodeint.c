57dc1fdd3f38fece5c0e9d38072bdf002cc0993e
KevinTMO <kevin.ramos3@hotmail.com>
2021-05-01 20:51:11 +0000

    Task 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -73,7 +73,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	}
 
 	new_node->next = tmpNode->next;
-	new_node->prev = tmpNode->prev;
+	tmpNode->prev = new_node->prev;
 	tmpNode->next = new_node;
 
 	return (new_node);
