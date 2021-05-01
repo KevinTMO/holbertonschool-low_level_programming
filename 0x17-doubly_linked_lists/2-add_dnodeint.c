3fa9d76ca5ec4ccba8fbbd1bdc9c7acddcbfbcfe
KevinTMO <kevin.ramos3@hotmail.com>
2021-05-01 16:09:07 +0000

    Header

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -24,7 +24,9 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	}
 
 	new_node->n = n;
+	new_node->next = NULL;
 	new_node->next = *head;
+	new_node->prev = NULL;
 	*head = new_node;
 
 	return (new_node);
