3f6ff56d2da49223d222d4e79a8350f0329f003a
KevinTMO <kevin.ramos3@hotmail.com>
2021-05-01 18:43:38 +0000

    Task 5

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+/**
+ *get_dnodeint_at_index - get the index of the node that we want
+ *@head: is the first node
+ *@index: is the position we want
+ *Return: will return head if we get the position or NULL if false
+ */
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int iterative = 0;
+
+	if (head == NULL)
+	{
+		return (NULL);
+	}
+
+	while (iterative < index && head != NULL)
+	{
+		iterative++;
+		head = head->next;
+	}
+
+	if (iterative == index)
+	{
+		return (head);
+	}
+	return (0);
+}
