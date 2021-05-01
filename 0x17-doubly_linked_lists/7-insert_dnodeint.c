226b6b0b3dc3fad6a2fd42a959e4e59a36e39cf4
KevinTMO <kevin.ramos3@hotmail.com>
2021-05-01 20:28:09 +0000

    Task 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,6 +1,29 @@
 #include "lists.h"
 
 /**
+ *dlistint_len - will get the lenght of the elements
+ *@h: is the first node
+ *Return: will return the lenght of the elements
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+        int count = 0;
+
+        if (h == NULL)
+        {
+                return (0);
+        }
+
+        while (h != NULL)
+        {
+                count++;
+                h = h->next;
+        }
+        return (count);
+}
+
+/**
  *insert_dnodeint_at_index - will insert a new node in the nth position
  *@h: will point to the first position
  *@idx: will be the position we need to get
@@ -43,6 +66,10 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			free(new_node);
 			return (NULL);
 		}
+	}
+	if (idx == dlistint_len(*h))
+	{
+		return (add_dnodeint_end(h, n));
 	}
 	new_node->next = tmpNode->next;
 	tmpNode->next = new_node;
