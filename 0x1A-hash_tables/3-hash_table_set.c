2e2de1f9badaef4e4acfe202d2d1e1fea0dabd28
KevinTMO <kevin.ramos3@hotmail.com>
2021-05-28 22:00:15 +0000

    Task 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -78,7 +78,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	node = (ht->array)[index];
 
-	while (node && (strcmp(key, node->key) == 1))
+	while (node && (strcmp(key, node->key) != 0))
 	{
 		node = node->next;
 	}
