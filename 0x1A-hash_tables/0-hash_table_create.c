6cdbcfaa6aadcd173ce103abae21b097026a41d8
KevinTMO <kevin.ramos3@hotmail.com>
2021-10-07 11:34:24 +0000

    Creating a hash_table

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,47 @@
+#include "hash_tables.h"
+
+/**
+ *
+ *
+ *
+ *
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *hash_table; /* will store values of arr and size */
+	hash_node_t **array; /* variable to create an array */
+	unsigned long int index; /* number to compare with size to create arr */
+
+	if (size == 0)
+	{
+		return (0);
+	}
+
+	hash_table = malloc(sizeof(hash_table_t));
+
+	if (hash_table == NULL)
+	{
+		return (0);
+	}
+
+	array = malloc(sizeof(*array) * size);
+
+	if (array == NULL)
+	{
+		return (0);
+	}
+
+	index = 0;
+
+	while (index < size)
+	{
+		*array = NULL;
+		index++;
+	}
+
+	hash_table->size = size;
+	hash_table->array = array;
+
+	return (hash_table);
+}
