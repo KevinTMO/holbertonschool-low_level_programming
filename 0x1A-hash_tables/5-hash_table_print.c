8ced8783eea9c903a229fce0a36e6a0095ba65b2
KevinTMO <kevin.ramos3@hotmail.com>
2021-10-08 11:11:44 +0000

    A fucntion print the hash table key/val pair

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,36 @@
+#include "hash_tables.h"
+
+/**
+ *hash_table_print - will print the hash table key/vals pair
+ *@ht: is the hash table
+ *Return: will return nothing (void)
+ */
+
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int index = 0;
+	hash_node_t *tmpNode;
+	char *comSpace = "";
+
+	if (!ht)
+	{
+		return;
+	}
+
+	putchar('{');
+
+	while (index < ht->size)
+	{
+		tmpNode = ht->array[index];
+
+		if (tmpNode != NULL)
+		{
+			printf("%s'%s':'%s'", comSpace, tmpNode->key,
+			       tmpNode->value);
+			comSpace = ", ";
+			tmpNode = tmpNode->next;
+		}
+		index++;
+	}
+	printf("}\n");
+}
