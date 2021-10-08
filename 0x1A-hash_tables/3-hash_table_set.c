751656cf195a7bf6cbd41d8a95026603184cdf31
KevinTMO <kevin.ramos3@hotmail.com>
2021-10-08 11:31:32 +0000

    Fixing betty style 40 lines

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -73,11 +73,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index;
 	char *copyVal;
 
-
 	if (!ht || !(ht->array) || !key || !value)
-	{
 		return (0);
-	}
 
 	index = key_index((const unsigned char *)key, ht->size);
 
