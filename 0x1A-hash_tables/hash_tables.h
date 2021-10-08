94ff49292d72e406df168d1549f5eded52ef9bd9
KevinTMO <kevin.ramos3@hotmail.com>
2021-10-08 09:28:42 +0000

    Updated

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -5,6 +5,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /* Structures to use below */
 
@@ -44,5 +45,12 @@ typedef struct hash_table_s
 hash_table_t *hash_table_create(unsigned long int size);
 unsigned long int hash_djb2(const unsigned char *str);
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
+int hash_table_set(hash_table_t *ht, const char *key, const char *value);
+char *hash_table_get(const hash_table_t *ht, const char *key);
+
+/* AUXFUNCTIONS BELOW */
+
+int create_node(hash_table_t *ht, const char *key, const char *value,
+		unsigned long int index);
 
 #endif /* HASH_TABLES_H */
