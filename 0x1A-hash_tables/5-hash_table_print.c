a8241d5e071c7815152ead7f1abfe2c336af4577
KevinTMO <kevin.ramos3@hotmail.com>
2021-10-08 11:13:33 +0000

    Adding a new char in string to fix output

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -25,7 +25,7 @@ void hash_table_print(const hash_table_t *ht)
 
 		if (tmpNode != NULL)
 		{
-			printf("%s'%s':'%s'", comSpace, tmpNode->key,
+			printf("%s'%s': '%s'", comSpace, tmpNode->key,
 			       tmpNode->value);
 			comSpace = ", ";
 			tmpNode = tmpNode->next;
