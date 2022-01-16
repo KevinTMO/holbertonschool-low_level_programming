680743c7164019c563d8305b1764592a495d25e8
KevinTMO <kevin.ramos3@hotmail.com>
2022-01-16 18:56:55 +0000

    Header

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,12 @@
+#ifndef _SEARCH_ALGOS_H
+#define _SEARCH_ALGOS_H
+
+/* LIBRARIES TO INCLUDE AND USE */
+
+#include <stdio.h>
+
+/* PROTOTYPES FUNCTIONS BELOW */
+
+int linear_search(int *array, size_t size, int value);
+
+#endif /* _SEARCH_ALGOS_H */
