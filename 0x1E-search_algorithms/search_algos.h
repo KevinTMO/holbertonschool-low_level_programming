88eca9349cf271f3fe834ffd0e6e93825b196e2e
KevinTMO <kevin.ramos3@hotmail.com>
2022-02-04 08:14:42 +0000

    Updated

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -4,14 +4,17 @@
 /* LIBRARIES TO INCLUDE AND USE */
 
 #include <stdio.h>
+#include <math.h>
 
 /* PROTOTYPES FUNCTIONS BELOW */
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
+int jump_search(int *array, size_t size, int value);
+int interpolation_search(int *array, size_t size, int value);
 
 /* HELPER FUNCTIONS */
 
-void printArr(int *array, int low, int high);
+void printArr(int *array, int low, int high, size_t mid);
 
 #endif /* _SEARCH_ALGOS_H */
